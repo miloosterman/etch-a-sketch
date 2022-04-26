73ea37c20253dbaeb740899fb3f2dc881c7f6da6
miloosterman <miloosterman@protonmail.com>
2022-04-26 14:20:15 -0700

    Added basic functionality for etch-a-sketch

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-let container = document.querySelector(".container");
+let grid = document.querySelector(".grid");
+let slider = document.getElementById("myRange");
+let button = document.querySelector(".clear-button");
+let sliderLabel = document.querySelector(".slider-label");
+let numSquares = slider.innerHTML.value;
 
-for (let i = 1; i <= 256; i++){
-  let squareDiv = document.createElement("div");
-  container.appendChild(squareDiv);
-  squareDiv.addEventListener("mouseover", () => squareDiv.style.backgroundColor = "black");
+let mouseDown = false;
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+slider.addEventListener("change", changeGridSize);
+button.addEventListener("click", resetGrid);
+
+
+setGrid(numSquares);
+
+function setGrid(numSquares = 256) {
+  for (let i = 1; i <= numSquares; i++){
+    let squareDiv = document.createElement("div");
+    squareDiv.classList.add("gridSquare");
+    grid.appendChild(squareDiv);
+    squareDiv.addEventListener("mouseover", setColor);
+  }
 }
 
-document.getElementsByClassName("div").addEv
+function setColor(e){
+  if (e.type === "mouseover" && !mouseDown) return
+  e.target.style.backgroundColor = "#FF10F0";
+}
+
+function resetGrid() {
+  let allSquares = document.querySelectorAll(".gridSquare");
+  for (let i = 1; i <= allSquares.length; i++){
+    if (allSquares[i]){
+      allSquares[i].style.backgroundColor = "black";
+    }
+  }
+}
+
+function changeGridSize(){
+  sliderLabel.textContent = `${slider.value} x ${slider.value}`;
+  setGrid(slider.value * slider.value);
+  grid.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
+  resetGrid();
+}
